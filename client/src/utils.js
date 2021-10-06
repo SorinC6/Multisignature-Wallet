b430b930127b544ba7c3d2d8fd422dbcce4d8684
SorinC6 <kish.sorin@yahoo.com>
2021-10-06 17:42:03 +0300

    added utils functions

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,2 +1,14 @@
 import Web3 from 'web3';
-import Wallet from ''
+import Wallet from './contracts/Wallet.json';
+
+const getWeb3 = () => {
+  return new Web3('http://127.0.0.1:9545/');
+};
+
+const getWallet = async (web3) => {
+  const networkId = await web3.eth.net.getId();
+  const deployedNetwork = Wallet.networks[networkId];
+  return new web3.eth.Contract(Wallet.abi, deployedNetwork && deployedNetwork.address);
+};
+
+export { getWallet, getWeb3 };
