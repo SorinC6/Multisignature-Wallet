a4f6e92a433941cd407a8f99bb160af0c1d915fe
SorinC6 <kish.sorin@yahoo.com>
2021-10-06 18:31:52 +0300

    read data from smart contrat

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import { getWeb3, getWallet } from './utils';
+import Header from './components/Header';
+
 function App() {
+  const [web3, setWeb3] = useState(undefined);
+  const [accounts, setAccounts] = useState(undefined);
+  const [wallet, setWallet] = useState(undefined);
+  const [approvers, setApprovers] = useState(undefined);
+  const [quorum, setQuorum] = useState(undefined);
+
+  useEffect(() => {
+    const init = async () => {
+      const web3 = getWeb3();
+      const accounts = await web3.eth.getAccounts();
+      const wallet = await getWallet(web3);
+
+      const approvers = await wallet.methods.getApprovers().call();
+      const quorum = await wallet.methods.quorum().call();
+      console.log('approvers', approvers);
+      console.log('quorum', quorum);
+
+      setWallet(wallet);
+      setAccounts(accounts);
+      setWeb3(web3);
+      setApprovers(approvers);
+      setQuorum(quorum);
+    };
+    init();
+  }, []);
+
+  if (
+    typeof web3 === 'undefined' ||
+    typeof accounts === 'undefined' ||
+    typeof approvers === 'undefined' ||
+    typeof wallet === 'undefined' ||
+    typeof quorum === 'undefined'
+  ) {
+    return <div>Loading...</div>;
+  }
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <h1>MultiSigApp [WIP]</h1>
+      <Header approvers={approvers} quorum={quorum} />
     </div>
   );
 }
