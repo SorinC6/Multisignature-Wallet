20ffe6886aa296cda07a4a91b1fd3bdca80bf244
SorinC6 <kish.sorin@yahoo.com>
2021-10-08 13:49:10 +0300

    added web3 for metamask config

--- client/src/utils.js
+++ client/src/utils.js
@@ -2,7 +2,26 @@ import Web3 from 'web3';
 import Wallet from './contracts/Wallet.json';
 
 const getWeb3 = () => {
-  return new Web3('http://127.0.0.1:9545/');
+  // used only locally
+  // return new Web3('http://127.0.0.1:9545/');
+
+  return new Promise(async (resolve, reject) => {
+    window.addEventListenet('load', async () => {
+      if (window.etherum) {
+        const web3 = new Web3(window.etherium);
+        resolve(web3);
+        try {
+          await window.etherium.enable();
+        } catch (error) {
+          reject(error);
+        }
+      } else if (window.web3) {
+        resolve(window.web3);
+      } else {
+        reject('Must install Metamask');
+      }
+    });
+  });
 };
 
 const getWallet = async (web3) => {
